4a62a168f9dc8c0b807a5e24912228e0b952ab86
wence <wence@LaptopWen>
2017-10-11 09:48:58 -0300

    Se agregaron las colecciones de: correlativas en asignaturas, historia en alumno y competencia en profesor con sus agregar y eliminar respectivos por clave primaria

--- TPFinal/src/modelo/Alumno.java
+++ TPFinal/src/modelo/Alumno.java
@@ -4,4 +4,20 @@ package modelo;
 public class Alumno
   extends Persona
 {
+  private IndicePrimario<String, Asignatura> historia;
+  
+  public Alumno()
+  {
+    this.historia=new IndicePrimario<String,Asignatura>();
+  }
+  
+  public void agregarHistoria(Asignatura nuevo)
+  {
+    this.historia.agregar(nuevo);
+  }
+  
+  public void eliminarHistoria(Asignatura elim)
+  {
+    this.historia.eliminar(elim);
+  }
 }
--- TPFinal/src/modelo/Profesor.java
+++ TPFinal/src/modelo/Profesor.java
@@ -5,12 +5,28 @@ public class Profesor
   extends Persona
 {
   private String telefono;
+  private IndicePrimario<String, Asignatura> competencia;
+
+  public Profesor()
+  {
+    this.competencia = new IndicePrimario<String, Asignatura>();
+  }
+
+  public void agregarCompetencia(Asignatura nuevo)
+  {
+    this.competencia.agregar(nuevo);
+  }
+  
+  public void eliminarCompetencia(Asignatura elim)
+  {
+    this.competencia.eliminar(elim);
+  }
 
   public void setTelefono(String telefono)
   {
     this.telefono = telefono;
   }
-
+  
   public String getTelefono()
   {
     return telefono;
