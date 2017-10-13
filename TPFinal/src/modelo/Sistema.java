d082343b7eed84504a241d5be6b79d7ef657c640
braulio <braulio@Presario>
2017-10-13 19:44:12 -0300

    Agregada DatoInvalidoException. Añadidos métodos de agregado en Sistema.

--- TPFinal/src/modelo/Sistema.java
+++ TPFinal/src/modelo/Sistema.java
@@ -1,5 +1,8 @@
 package modelo;
 
+import excepciones.ClaveYaExistenteException;
+import excepciones.DatoInvalidoException;
+
 
 public class Sistema
 {
@@ -8,6 +11,14 @@ public class Sistema
     private IndiceDoble<Asignatura> planDeEstudio;
     private IndiceDoble<Cursada> calendario;
 
+    public Sistema()
+    {
+        this.alumnos = new IndiceDoble<Alumno>();
+        this.profesores = new IndiceDoble<Profesor>();
+        this.planDeEstudio = new IndiceDoble<Asignatura>();
+        this.calendario = new IndiceDoble<Cursada>();
+    }
+    
     public void setAlumnos(IndiceDoble<Alumno> alumnos)
     {
         this.alumnos = alumnos;
@@ -46,5 +57,40 @@ public class Sistema
     public IndiceDoble<Cursada> getCalendario()
     {
         return calendario;
+    }
+    
+    public void agregarAlumno(Alumno nuevo)
+        throws ClaveYaExistenteException, DatoInvalidoException
+    {
+        if (!Persona.validaMail(nuevo.getMail()))
+            throw new DatoInvalidoException(nuevo.getMail(), "Mail inválido.");
+        else
+            this.alumnos.agregar(nuevo);
+    }
+    
+    public void agregarProfesor(Profesor nuevo)
+        throws ClaveYaExistenteException, DatoInvalidoException
+    {
+        if (!Persona.validaMail(nuevo.getMail()))
+            throw new DatoInvalidoException(nuevo.getMail(), "Mail inválido.");
+        else
+            this.profesores.agregar(nuevo);
+    }
+    
+    public void agregarAsignatura(Asignatura nuevo)
+        throws ClaveYaExistenteException
+    {
+        this.planDeEstudio.agregar(nuevo);
+    }
+    
+    public void agregarCursada(Cursada nuevo)
+        throws ClaveYaExistenteException, DatoInvalidoException
+    {
+        if (!Cursada.validaHora(nuevo.getHora()))
+            throw new DatoInvalidoException(nuevo.getHora(), "Hora inválida.");
+        else if (!Cursada.validaPeriodo(nuevo.getPeriodo()))
+            throw new DatoInvalidoException(nuevo.getPeriodo(), "Periodo inválido.");
+        else
+            this.calendario.agregar(nuevo);
     }
 }
