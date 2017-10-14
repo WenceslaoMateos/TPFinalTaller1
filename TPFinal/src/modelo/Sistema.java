7e4484f3ea34cc58e98500e16aabc376091383de
braulio <braulio@Presario>
2017-10-14 16:27:11 -0300

    Agregados métodos para eliminar personas y asignaturas del sistema.

--- TPFinal/src/modelo/Sistema.java
+++ TPFinal/src/modelo/Sistema.java
@@ -3,6 +3,8 @@ package modelo;
 import excepciones.ClaveYaExistenteException;
 import excepciones.DatoInvalidoException;
 
+import java.util.Iterator;
+
 
 public class Sistema
 {
@@ -93,5 +95,65 @@ public class Sistema
         // TODO faltan verificaciones
         else
             this.calendario.agregar(nuevo);
+    }
+    
+    public void eliminarAlumno(Alumno elim)
+    {
+        Cursada aux;
+        Iterator<Cursada> it;
+        this.alumnos.eliminar(elim);
+        it = this.calendario.elementosPorClavePrimaria();
+        while (it.hasNext())
+        {
+            aux = it.next();
+            if (aux.getAlumnos().contieneValor(elim))
+                aux.getAlumnos().eliminar(elim);
+        }
+    }
+    
+    public void eliminarProfesor(Profesor elim)
+    {
+        Cursada aux;
+        Iterator<Cursada> it;
+        this.profesores.eliminar(elim);
+        it = this.calendario.elementosPorClavePrimaria();
+        while (it.hasNext())
+        {
+            aux = it.next();
+            if (aux.getProfesores().contieneValor(elim))
+                aux.getProfesores().eliminar(elim);
+        }
+    }
+    
+    public void eliminarAsignatura(Asignatura elim)
+    {
+        this.planDeEstudio.eliminar(elim);
+        this.eliminaAsignaturaEnAlumnos(elim);
+        this.eliminaAsignaturaEnProfesores(elim);
+        // TODO eliminar cursadas asociadas a la asignatura
+    }
+    
+    private void eliminaAsignaturaEnAlumnos(Asignatura elim)
+    {
+        Iterator<Alumno> it = this.alumnos.elementosPorClavePrimaria();
+        Alumno aux;
+        while (it.hasNext())
+        {
+            aux = it.next();
+            if (aux.getHistoria().contieneValor(elim))
+                aux.eliminarHistoria(elim);
+        }
+    }
+    
+    private void eliminaAsignaturaEnProfesores(Asignatura elim)
+    {
+        Iterator<Profesor> it = this.profesores.elementosPorClavePrimaria();
+        Profesor aux;
+        while (it.hasNext())
+        {
+            aux = it.next();
+            if (aux.habilitadoParaAsignatura(elim))
+                aux.eliminarCompetencia(elim);
+        }
     }
 }
