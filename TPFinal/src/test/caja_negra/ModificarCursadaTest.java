cbe7020492c2c34c2835182b2db66f34cd6b3bf7
braulio <braulio@Presario>
2017-11-18 12:05:05 -0300

    Más pulido

--- TPFinal/src/test/caja_negra/ModificarCursadaTest.java
+++ TPFinal/src/test/caja_negra/ModificarCursadaTest.java
@@ -16,227 +16,227 @@ import org.junit.Test;
 
 public class ModificarCursadaTest
 {
-  private Sistema sistema;
-  private Cursada cursada;
-  private Asignatura asignatura;
-  private Asignatura otraAsignatura;
+    private Sistema sistema;
+    private Cursada cursada;
+    private Asignatura asignatura;
+    private Asignatura otraAsignatura;
 
-  public ModificarCursadaTest()
-  {
-  }
+    public ModificarCursadaTest()
+    {
+    }
 
-  @Before
-  public void setUp()
-    throws Exception
-  {
-    this.sistema = new Sistema();
-    this.asignatura = new Asignatura("Física 1");
-    this.sistema.agregarAsignatura(this.asignatura);
-    this.otraAsignatura = new Asignatura("Otra");
-    this.sistema.agregarAsignatura(this.otraAsignatura);
-    this.cursada = new Cursada(this.asignatura, "02-2017", Dia.LUN, "12:00", "14:00");
-    this.sistema.agregarCursada(this.cursada);
-  }
+    @Before
+    public void setUp()
+        throws Exception
+    {
+        this.sistema = new Sistema();
+        this.asignatura = new Asignatura("Física 1");
+        this.sistema.agregarAsignatura(this.asignatura);
+        this.otraAsignatura = new Asignatura("Otra");
+        this.sistema.agregarAsignatura(this.otraAsignatura);
+        this.cursada = new Cursada(this.asignatura, "02-2017", Dia.LUN, "12:00", "14:00");
+        this.sistema.agregarCursada(this.cursada);
+    }
 
-  @After
-  public void tearDown()
-    throws Exception
-  {
-    this.sistema = null;
-    this.asignatura = null;
-    this.cursada = null;
-  }
+    @After
+    public void tearDown()
+        throws Exception
+    {
+        this.sistema = null;
+        this.asignatura = null;
+        this.cursada = null;
+    }
 
-  /**
-   * @see Sistema#modificarCursada(Cursada,Cursada)
-   */
-  @Test
-  public void testModificarCursadaCorrecta()
-  {
-    Cursada modif = new Cursada(this.otraAsignatura, "01-2017", Dia.DOM, "11:00", "13:00");
-    try
+    /**
+     * @see Sistema#modificarCursada(Cursada,Cursada)
+     */
+    @Test
+    public void testModificarCursadaCorrecta()
     {
-      this.sistema.modificarCursada(this.cursada, modif);
+        Cursada modif = new Cursada(this.otraAsignatura, "01-2017", Dia.DOM, "11:00", "13:00");
+        try
+        {
+            this.sistema.modificarCursada(this.cursada, modif);
+        }
+        catch (DatoInvalidoException e)
+        {
+            Assert.fail("Los datos deberian ser invalidos");
+        }
+        Assert.assertSame("La asignatura deberia estar modificada", this.otraAsignatura, this.cursada.getAsignatura());
+        Assert.assertTrue("El periodo deberia estar modificado", this.cursada
+                                                                     .getPeriodo()
+                                                                     .equals("01-2017"));
+        Assert.assertSame("La asignatura deberia estar modificada", Dia.DOM, this.cursada.getDia());
+        Assert.assertTrue("La hora de inicio deberia estar modificada", this.cursada
+                                                                            .getHoraInicio()
+                                                                            .equals("11:00"));
+        Assert.assertTrue("La hora de finalización deberia estar modificada", this.cursada
+                                                                                  .getHoraFinalizacion()
+                                                                                  .equals("13:00"));
     }
-    catch (DatoInvalidoException e)
-    {
-      Assert.fail("Los datos deberian ser invalidos");
-    }
-    Assert.assertSame("La asignatura deberia estar modificada", this.otraAsignatura, this.cursada.getAsignatura());
-    Assert.assertTrue("El periodo deberia estar modificado", this.cursada
-                                                                 .getPeriodo()
-                                                                 .equals("01-2017"));
-    Assert.assertSame("La asignatura deberia estar modificada", Dia.DOM, this.cursada.getDia());
-    Assert.assertTrue("La hora de inicio deberia estar modificada", this.cursada
-                                                                        .getHoraInicio()
-                                                                        .equals("11:00"));
-    Assert.assertTrue("La hora de finalización deberia estar modificada", this.cursada
-                                                                              .getHoraFinalizacion()
-                                                                              .equals("13:00"));
-  }
 
-  /**
-   * @see Sistema#modificarCursada(Cursada,Cursada)
-   */
-  @Test
-  public void testModificarCursadaErronea2()
-  {
-    Cursada modif = new Cursada(this.otraAsignatura, "01-2017", Dia.DOM, "11:00", "13:00");
-    try
+    /**
+     * @see Sistema#modificarCursada(Cursada,Cursada)
+     */
+    @Test
+    public void testModificarCursadaErronea2()
     {
-      this.sistema.modificarCursada(null, modif);
+        Cursada modif = new Cursada(this.otraAsignatura, "01-2017", Dia.DOM, "11:00", "13:00");
+        try
+        {
+            this.sistema.modificarCursada(null, modif);
+        }
+        catch (DatoInvalidoException e)
+        {
+            Assert.fail("Los datos no deberian ser invalidos");
+        }
+        catch (Exception e)
+        {
+        }
     }
-    catch (DatoInvalidoException e)
-    {
-      Assert.fail("Los datos no deberian ser invalidos");
-    }
-    catch (Exception e)
-    {
-    }
-  }
 
-  /**
-   * @see Sistema#modificarCursada(Cursada,Cursada)
-   */
-  @Test
-  public void testModificarCursadaErronea4_1()
-  {
-    try
+    /**
+     * @see Sistema#modificarCursada(Cursada,Cursada)
+     */
+    @Test
+    public void testModificarCursadaErronea4_1()
     {
-      this.sistema.modificarCursada(this.cursada, null);
+        try
+        {
+            this.sistema.modificarCursada(this.cursada, null);
+        }
+        catch (DatoInvalidoException e)
+        {
+            Assert.fail("Los datos no deberian ser invalidos");
+        }
+        catch (Exception e)
+        {
+        }
     }
-    catch (DatoInvalidoException e)
-    {
-      Assert.fail("Los datos no deberian ser invalidos");
-    }
-    catch (Exception e)
-    {
-    }
-  }
 
-  /**
-   * @see Sistema#modificarCursada(Cursada,Cursada)
-   */
-  @Test
-  public void testModificarCursadaErronea4_2()
-  {
-    Cursada modif = new Cursada(this.otraAsignatura, "ab-2017", Dia.DOM, "11:00", "13:00");
-    try
+    /**
+     * @see Sistema#modificarCursada(Cursada,Cursada)
+     */
+    @Test
+    public void testModificarCursadaErronea4_2()
     {
-      this.sistema.modificarCursada(this.cursada, modif);
+        Cursada modif = new Cursada(this.otraAsignatura, "ab-2017", Dia.DOM, "11:00", "13:00");
+        try
+        {
+            this.sistema.modificarCursada(this.cursada, modif);
+        }
+        catch (DatoInvalidoException e)
+        {
+        }
     }
-    catch (DatoInvalidoException e)
-    {
-    }
-  }
 
-  /**
-   * @see Sistema#modificarCursada(Cursada,Cursada)
-   */
-  @Test
-  public void testModificarCursadaErronea4_3()
-  {
-    Cursada modif = new Cursada(this.otraAsignatura, "01-2017", Dia.DOM, "ab:00", "13:00");
-    try
+    /**
+     * @see Sistema#modificarCursada(Cursada,Cursada)
+     */
+    @Test
+    public void testModificarCursadaErronea4_3()
     {
-      this.sistema.modificarCursada(this.cursada, modif);
+        Cursada modif = new Cursada(this.otraAsignatura, "01-2017", Dia.DOM, "ab:00", "13:00");
+        try
+        {
+            this.sistema.modificarCursada(this.cursada, modif);
+        }
+        catch (DatoInvalidoException e)
+        {
+        }
     }
-    catch (DatoInvalidoException e)
-    {
-    }
-  }
 
-  /**
-   * @see Sistema#modificarCursada(Cursada,Cursada)
-   */
-  @Test
-  public void testModificarCursadaErronea4_4()
-  {
-    Cursada modif = new Cursada(this.otraAsignatura, "01-2017", Dia.DOM, "11:00", "ab:00");
-    try
+    /**
+     * @see Sistema#modificarCursada(Cursada,Cursada)
+     */
+    @Test
+    public void testModificarCursadaErronea4_4()
     {
-      this.sistema.modificarCursada(this.cursada, modif);
+        Cursada modif = new Cursada(this.otraAsignatura, "01-2017", Dia.DOM, "11:00", "ab:00");
+        try
+        {
+            this.sistema.modificarCursada(this.cursada, modif);
+        }
+        catch (DatoInvalidoException e)
+        {
+        }
     }
-    catch (DatoInvalidoException e)
-    {
-    }
-  }
 
-  /**
-   * @see Sistema#modificarCursada(Cursada,Cursada)
-   */
-  @Test
-  public void testModificarCursadaErronea4_5()
-  {
-    Cursada modif = new Cursada(this.otraAsignatura, "01-2017", Dia.DOM, "11:00", "10:00");
-    try
+    /**
+     * @see Sistema#modificarCursada(Cursada,Cursada)
+     */
+    @Test
+    public void testModificarCursadaErronea4_5()
     {
-      this.sistema.modificarCursada(this.cursada, modif);
+        Cursada modif = new Cursada(this.otraAsignatura, "01-2017", Dia.DOM, "11:00", "10:00");
+        try
+        {
+            this.sistema.modificarCursada(this.cursada, modif);
+        }
+        catch (DatoInvalidoException e)
+        {
+        }
     }
-    catch (DatoInvalidoException e)
-    {
-    }
-  }
 
-  /**
-   * @see Sistema#modificarCursada(Cursada,Cursada)
-   */
-  @Test
-  public void testModificarCursadaErronea4_6()
-  {
-    Cursada modif = new Cursada(new Asignatura(""), "01-2017", Dia.DOM, "11:00", "13:00");
-    try
+    /**
+     * @see Sistema#modificarCursada(Cursada,Cursada)
+     */
+    @Test
+    public void testModificarCursadaErronea4_6()
     {
-      this.sistema.modificarCursada(this.cursada, modif);
+        Cursada modif = new Cursada(new Asignatura(""), "01-2017", Dia.DOM, "11:00", "13:00");
+        try
+        {
+            this.sistema.modificarCursada(this.cursada, modif);
+        }
+        catch (DatoInvalidoException e)
+        {
+            Assert.fail("Los datos no deberian ser invalidos");
+        }
+        catch (Exception e)
+        {
+        }
     }
-    catch (DatoInvalidoException e)
-    {
-      Assert.fail("Los datos no deberian ser invalidos");
-    }
-    catch (Exception e)
-    {
-    }
-  }
 
-  /**
-   * @see Sistema#modificarCursada(Cursada,Cursada)
-   */
-  @Test
-  public void testModificarCursadaErronea6()
-  {
-    this.sistema.setCalendario(new IndiceDoble<Cursada>());
-    Cursada modif = new Cursada(this.otraAsignatura, "01-2017", Dia.DOM, "11:00", "13:00");
-    try
+    /**
+     * @see Sistema#modificarCursada(Cursada,Cursada)
+     */
+    @Test
+    public void testModificarCursadaErronea6()
     {
-      this.sistema.modificarCursada(this.cursada, modif);
+        this.sistema.setCalendario(new IndiceDoble<Cursada>());
+        Cursada modif = new Cursada(this.otraAsignatura, "01-2017", Dia.DOM, "11:00", "13:00");
+        try
+        {
+            this.sistema.modificarCursada(this.cursada, modif);
+        }
+        catch (DatoInvalidoException e)
+        {
+            Assert.fail("Los datos no deberian ser invalidos");
+        }
+        catch (Exception e)
+        {
+        }
     }
-    catch (DatoInvalidoException e)
-    {
-      Assert.fail("Los datos no deberian ser invalidos");
-    }
-    catch (Exception e)
-    {
-    }
-  }
 
-  /**
-   * @see Sistema#modificarCursada(Cursada,Cursada)
-   */
-  @Test
-  public void testModificarCursadaErronea8()
-  {
-    this.sistema.eliminarAsignatura(this.otraAsignatura);
-    Cursada modif = new Cursada(this.otraAsignatura, "01-2017", Dia.DOM, "11:00", "13:00");
-    try
+    /**
+     * @see Sistema#modificarCursada(Cursada,Cursada)
+     */
+    @Test
+    public void testModificarCursadaErronea8()
     {
-      this.sistema.modificarCursada(this.cursada, modif);
+        this.sistema.eliminarAsignatura(this.otraAsignatura);
+        Cursada modif = new Cursada(this.otraAsignatura, "01-2017", Dia.DOM, "11:00", "13:00");
+        try
+        {
+            this.sistema.modificarCursada(this.cursada, modif);
+        }
+        catch (DatoInvalidoException e)
+        {
+            Assert.fail("Los datos no deberian ser invalidos");
+        }
+        catch (Exception e)
+        {
+        }
     }
-    catch (DatoInvalidoException e)
-    {
-      Assert.fail("Los datos no deberian ser invalidos");
-    }
-    catch (Exception e)
-    {
-    }
-  }
 }
--- TPFinal/src/test/caja_negra/QuitarCompetenciaAProfesorTest.java
+++ TPFinal/src/test/caja_negra/QuitarCompetenciaAProfesorTest.java
@@ -2,185 +2,180 @@ package test.caja_negra;
 
 import org.junit.After;
 import org.junit.Assert;
-import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
 public class QuitarCompetenciaAProfesorTest
 {
-  SistemaConElementos fixture1 = new SistemaConElementos();
+    SistemaConElementosFixture fixture1 = new SistemaConElementosFixture();
 
-  public QuitarCompetenciaAProfesorTest()
-  {
-  }
+    public QuitarCompetenciaAProfesorTest()
+    {
+    }
 
-  @Before
-  public void setUp()
-    throws Exception
-  {
-    fixture1.setUp();
-  }
+    @Before
+    public void setUp()
+        throws Exception
+    {
+        fixture1.setUp();
+    }
 
-  @After
-  public void tearDown()
-    throws Exception
-  {
-    fixture1.tearDown();
-  }
+    @After
+    public void tearDown()
+        throws Exception
+    {
+        fixture1.tearDown();
+    }
 
-  /**
-   * @see modelo.Sistema#quitarCompetenciaAProfesor(modelo.Profesor,modelo.Asignatura)
-   */
-  @Test
-  public void testQuitarCompetenciaAProfesorCorrecto()
-  {
-    this.fixture1
-        .sistema
-        .quitarCompetenciaAProfesor(this.fixture1.profesor, this.fixture1.asignatura);
-    Assert.assertFalse("No se quitó correctamente la asignatura al profesor",
-                       this.fixture1
-                                                                                  .profesor
-                                                                                  .habilitadoParaAsignatura(this.fixture1.asignatura));
-  }
+    /**
+     * @see modelo.Sistema#quitarCompetenciaAProfesor(modelo.Profesor,modelo.Asignatura)
+     */
+    @Test
+    public void testQuitarCompetenciaAProfesorCorrecto()
+    {
+        this.fixture1.sistema.quitarCompetenciaAProfesor(this.fixture1.profesor, this.fixture1.asignatura1);
+        Assert.assertFalse("No se quitó correctamente la asignatura al profesor",
+                           this.fixture1.profesor.habilitadoParaAsignatura(this.fixture1.asignatura1));
+    }
 
-  /**
-   * @see modelo.Sistema#quitarCompetenciaAProfesor(modelo.Profesor,modelo.Asignatura)
-   */
-  @Test
-  public void testQuitarCompetenciaAProfesorErroneo2()
-  {
-    try
+    /**
+     * @see modelo.Sistema#quitarCompetenciaAProfesor(modelo.Profesor,modelo.Asignatura)
+     */
+    @Test
+    public void testQuitarCompetenciaAProfesorErroneo2()
     {
-      this.fixture1
-          .sistema
-          .quitarCompetenciaAProfesor(null, this.fixture1.asignatura);
-      Assert.fail("No deberia haber podido terminar la ejecución del metodo");
+        try
+        {
+            this.fixture1
+                .sistema
+                .quitarCompetenciaAProfesor(null, this.fixture1.asignatura1);
+            Assert.fail("No deberia haber podido terminar la ejecución del metodo");
+        }
+        catch (Exception e)
+        {
+        }
     }
-    catch (Exception e)
-    {
-    }
-  }
 
-  /**
-   * @see modelo.Sistema#quitarCompetenciaAProfesor(modelo.Profesor,modelo.Asignatura)
-   */
-  @Test
-  public void testQuitarCompetenciaAProfesorErroneo4()
-  {
-    try
+    /**
+     * @see modelo.Sistema#quitarCompetenciaAProfesor(modelo.Profesor,modelo.Asignatura)
+     */
+    @Test
+    public void testQuitarCompetenciaAProfesorErroneo4()
     {
-      this.fixture1
-          .sistema
-          .quitarCompetenciaAProfesor(this.fixture1.profesor, null);
-      Assert.fail("No deberia haber podido terminar la ejecución del metodo");
+        try
+        {
+            this.fixture1
+                .sistema
+                .quitarCompetenciaAProfesor(this.fixture1.profesor, null);
+            Assert.fail("No deberia haber podido terminar la ejecución del metodo");
+        }
+        catch (Exception e)
+        {
+        }
     }
-    catch (Exception e)
-    {
-    }
-  }
 
-  /**
-   * @see modelo.Sistema#quitarCompetenciaAProfesor(modelo.Profesor,modelo.Asignatura)
-   */
-  @Test
-  public void testQuitarCompetenciaAProfesorErroneo6()
-  {
-    try
+    /**
+     * @see modelo.Sistema#quitarCompetenciaAProfesor(modelo.Profesor,modelo.Asignatura)
+     */
+    @Test
+    public void testQuitarCompetenciaAProfesorErroneo6()
     {
-      this.fixture1
-          .sistema
-          .setCalendario(null);
-      this.fixture1
-          .sistema
-          .quitarCompetenciaAProfesor(this.fixture1.profesor, this.fixture1.asignatura);
-      Assert.fail("No deberia haber podido terminar la ejecución del metodo");
+        try
+        {
+            this.fixture1
+                .sistema
+                .setCalendario(null);
+            this.fixture1
+                .sistema
+                .quitarCompetenciaAProfesor(this.fixture1.profesor, this.fixture1.asignatura1);
+            Assert.fail("No deberia haber podido terminar la ejecución del metodo");
+        }
+        catch (Exception e)
+        {
+        }
     }
-    catch (Exception e)
-    {
-    }
-  }
 
-  /**
-   * @see modelo.Sistema#quitarCompetenciaAProfesor(modelo.Profesor,modelo.Asignatura)
-   */
-  @Test
-  public void testQuitarCompetenciaAProfesorErroneo8()
-  {
-    try
+    /**
+     * @see modelo.Sistema#quitarCompetenciaAProfesor(modelo.Profesor,modelo.Asignatura)
+     */
+    @Test
+    public void testQuitarCompetenciaAProfesorErroneo8()
     {
-      this.fixture1
-          .profesor
-          .setCompetencia(null);
-      this.fixture1
-          .sistema
-          .quitarCompetenciaAProfesor(this.fixture1.profesor, this.fixture1.asignatura);
-      Assert.fail("No deberia haber podido terminar la ejecución del metodo");
+        try
+        {
+            this.fixture1
+                .profesor
+                .setCompetencia(null);
+            this.fixture1
+                .sistema
+                .quitarCompetenciaAProfesor(this.fixture1.profesor, this.fixture1.asignatura1);
+            Assert.fail("No deberia haber podido terminar la ejecución del metodo");
+        }
+        catch (Exception e)
+        {
+        }
     }
-    catch (Exception e)
-    {
-    }
-  }
 
-  /**
-   * @see modelo.Sistema#quitarCompetenciaAProfesor(modelo.Profesor,modelo.Asignatura)
-   */
-  @Test
-  public void testQuitarCompetenciaAProfesorErroneo10()
-  {
-    try
+    /**
+     * @see modelo.Sistema#quitarCompetenciaAProfesor(modelo.Profesor,modelo.Asignatura)
+     */
+    @Test
+    public void testQuitarCompetenciaAProfesorErroneo10()
     {
-      this.fixture1
-          .sistema
-          .setProfesores(null);
-      this.fixture1
-          .sistema
-          .quitarCompetenciaAProfesor(this.fixture1.profesor, this.fixture1.asignatura);
-      Assert.fail("No deberia haber podido terminar la ejecución del metodo");
+        try
+        {
+            this.fixture1
+                .sistema
+                .setProfesores(null);
+            this.fixture1
+                .sistema
+                .quitarCompetenciaAProfesor(this.fixture1.profesor, this.fixture1.asignatura1);
+            Assert.fail("No deberia haber podido terminar la ejecución del metodo");
+        }
+        catch (Exception e)
+        {
+        }
     }
-    catch (Exception e)
-    {
-    }
-  }
 
-  /**
-   * @see modelo.Sistema#quitarCompetenciaAProfesor(modelo.Profesor,modelo.Asignatura)
-   */
-  @Test
-  public void testQuitarCompetenciaAProfesorErroneo12()
-  {
-    try
+    /**
+     * @see modelo.Sistema#quitarCompetenciaAProfesor(modelo.Profesor,modelo.Asignatura)
+     */
+    @Test
+    public void testQuitarCompetenciaAProfesorErroneo12()
     {
-      this.fixture1
-          .sistema
-          .quitarCompetenciaAProfesor(this.fixture1.profesor, this.fixture1.asignatura);
-      this.fixture1
-          .sistema
-          .quitarCompetenciaAProfesor(this.fixture1.profesor, this.fixture1.asignatura);
-      Assert.fail("No deberia haber podido terminar la ejecución del metodo");
+        try
+        {
+            this.fixture1
+                .sistema
+                .quitarCompetenciaAProfesor(this.fixture1.profesor, this.fixture1.asignatura1);
+            this.fixture1
+                .sistema
+                .quitarCompetenciaAProfesor(this.fixture1.profesor, this.fixture1.asignatura1);
+            Assert.fail("No deberia haber podido terminar la ejecución del metodo");
+        }
+        catch (Exception e)
+        {
+        }
     }
-    catch (Exception e)
-    {
-    }
-  }
 
-  /**
-   * @see modelo.Sistema#quitarCompetenciaAProfesor(modelo.Profesor,modelo.Asignatura)
-   */
-  @Test
-  public void testQuitarCompetenciaAProfesorErroneo14()
-  {
-    try
+    /**
+     * @see modelo.Sistema#quitarCompetenciaAProfesor(modelo.Profesor,modelo.Asignatura)
+     */
+    @Test
+    public void testQuitarCompetenciaAProfesorErroneo14()
     {
-      this.fixture1
-          .sistema
-          .eliminarProfesor(this.fixture1.profesor);
-      this.fixture1
-          .sistema
-          .quitarCompetenciaAProfesor(this.fixture1.profesor, this.fixture1.asignatura);
-      Assert.fail("No deberia haber podido terminar la ejecución del metodo");
+        try
+        {
+            this.fixture1
+                .sistema
+                .eliminarProfesor(this.fixture1.profesor);
+            this.fixture1
+                .sistema
+                .quitarCompetenciaAProfesor(this.fixture1.profesor, this.fixture1.asignatura1);
+            Assert.fail("No deberia haber podido terminar la ejecución del metodo");
+        }
+        catch (Exception e)
+        {
+        }
     }
-    catch (Exception e)
-    {
-    }
-  }
 }
