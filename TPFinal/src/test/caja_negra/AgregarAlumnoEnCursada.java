481cd11ea56a8b974bebc2b3f94aac8fc373a223
wence <wence@LaptopWen>
2017-11-18 11:08:55 -0300

    Test de alta y baja de alumnos dentro de asignatura completos

---
+++ TPFinal/src/test/caja_negra/AgregarAlumnoEnCursada.java
@@ -0,0 +1,247 @@
+package test.caja_negra;
+
+import excepciones.ClaveYaExistenteException;
+import excepciones.DatoInvalidoException;
+
+import modelo.Cursada;
+import modelo.IndiceDoble;
+
+import org.junit.After;
+import org.junit.Assert;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AgregarAlumnoEnCursada
+{
+  SistemaConElementos fixture1 = new SistemaConElementos();
+
+  public AgregarAlumnoEnCursada()
+  {
+  }
+
+  @Before
+  public void setUp()
+    throws Exception
+  {
+    fixture1.setUp();
+    this.fixture1
+        .alumno
+        .eliminarHistoria(this.fixture1.asignatura);
+  }
+
+  @After
+  public void tearDown()
+    throws Exception
+  {
+    fixture1.tearDown();
+  }
+
+  /**
+   * @see modelo.Sistema#agregarAlumnoEnCursada(modelo.Alumno,modelo.Cursada)
+   */
+  @Test
+  public void testAgregarAlumnoEnCursadaCorrecto()
+  {
+    try
+    {
+      this.fixture1
+          .sistema
+          .agregarAlumnoEnCursada(this.fixture1.alumno, this.fixture1.cursada);
+    }
+    catch (ClaveYaExistenteException e)
+    {
+      Assert.fail("La clave no deberia existir");
+    }
+    catch (DatoInvalidoException e)
+    {
+      Assert.fail("El dato no deberia ser invalido");
+    }
+  }
+
+  /**
+   * @see modelo.Sistema#agregarAlumnoEnCursada(modelo.Alumno,modelo.Cursada)
+   */
+  @Test
+  public void testAgregarAlumnoEnCursadaErroneo2()
+  {
+    try
+    {
+      this.fixture1
+          .sistema
+          .agregarAlumnoEnCursada(null, this.fixture1.cursada);
+      Assert.fail("El alumno no deberia ser agregado a la cursada");
+    }
+    catch (ClaveYaExistenteException e)
+    {
+      Assert.fail("La clave no deberia existir");
+    }
+    catch (DatoInvalidoException e)
+    {
+      Assert.fail("El dato no deberia ser invalido");
+    }
+    catch (Exception e)
+    {
+    }
+  }
+
+  /**
+   * @see modelo.Sistema#agregarAlumnoEnCursada(modelo.Alumno,modelo.Cursada)
+   */
+  @Test
+  public void testAgregarAlumnoEnCursadaErroneo4_1()
+  {
+    try
+    {
+      this.fixture1
+          .sistema
+          .agregarAlumnoEnCursada(this.fixture1.alumno, null);
+      Assert.fail("El alumno no deberia ser agregado a la cursada");
+    }
+    catch (ClaveYaExistenteException e)
+    {
+      Assert.fail("La clave no deberia existir");
+    }
+    catch (DatoInvalidoException e)
+    {
+      Assert.fail("El dato no deberia ser invalido");
+    }
+    catch (Exception e)
+    {
+    }
+  }
+
+  /**
+   * @see modelo.Sistema#agregarAlumnoEnCursada(modelo.Alumno,modelo.Cursada)
+   */
+  @Test
+  public void testAgregarAlumnoEnCursadaErroneo4_2()
+  {
+    try
+    {
+      this.fixture1
+          .sistema
+          .agregarAlumnoEnCursada(this.fixture1.alumno, this.fixture1.cursada);
+    }
+    catch (ClaveYaExistenteException e)
+    {
+      Assert.fail("La clave no deberia existir");
+    }
+    catch (DatoInvalidoException e)
+    {
+      Assert.fail("El dato no deberia ser invalido");
+    }
+    catch (Exception e)
+    {
+      Assert.fail("Deberia agregarlo correctamente.");
+    }
+    try
+    {
+      this.fixture1
+          .sistema
+          .agregarAlumnoEnCursada(this.fixture1.alumno, this.fixture1.cursada);
+      Assert.fail("No deberia poder agregar al alumno");
+    }
+    catch (ClaveYaExistenteException e)
+    {
+    }
+    catch (DatoInvalidoException e)
+    {
+      Assert.fail("El dato no deberia ser invalido");
+    }
+    catch (Exception e)
+    {
+      Assert.fail("No deberia terminar por este error");
+    }
+  }
+
+  /**
+   * @see modelo.Sistema#agregarAlumnoEnCursada(modelo.Alumno,modelo.Cursada)
+   */
+  @Test
+  public void testAgregarAlumnoEnCursadaErroneo6()
+  {
+    try
+    {
+      this.fixture1
+          .alumno
+          .agregarHistoria(this.fixture1.asignatura);
+    }
+    catch (ClaveYaExistenteException e)
+    {
+      Assert.fail("No deberia existir la asignatura previamente.");
+    }
+    try
+    {
+      this.fixture1
+          .sistema
+          .agregarAlumnoEnCursada(this.fixture1.alumno, this.fixture1.cursada);
+      Assert.fail("No deberia poder agregar el alumno a la cursada");
+    }
+    catch (ClaveYaExistenteException e)
+    {
+      Assert.fail("La clave no deberia existir");
+    }
+    catch (DatoInvalidoException e)
+    {
+    }
+  }
+
+  /**
+   * @see modelo.Sistema#agregarAlumnoEnCursada(modelo.Alumno,modelo.Cursada)
+   */
+  @Test
+  public void testAgregarAlumnoEnCursadaErroneo8()
+  {
+    this.fixture1
+        .sistema
+        .setCalendario(null);
+    try
+    {
+      this.fixture1
+          .sistema
+          .agregarAlumnoEnCursada(this.fixture1.alumno, this.fixture1.cursada);
+      Assert.fail("No deberia poder agregar el alumno a la cursada");
+    }
+    catch (ClaveYaExistenteException e)
+    {
+      Assert.fail("La clave no deberia existir");
+    }
+    catch (DatoInvalidoException e)
+    {
+      Assert.fail("El dato no deberia ser invalido");
+    }
+    catch (Exception e)
+    {
+    }
+  }
+
+  /**
+   * @see modelo.Sistema#agregarAlumnoEnCursada(modelo.Alumno,modelo.Cursada)
+   */
+  @Test
+  public void testAgregarAlumnoEnCursadaErroneo10()
+  {
+    this.fixture1
+        .sistema
+        .setCalendario(new IndiceDoble<Cursada>());
+    try
+    {
+      this.fixture1
+          .sistema
+          .agregarAlumnoEnCursada(this.fixture1.alumno, this.fixture1.cursada);
+      Assert.fail("No deberia poder agregar el alumno a la cursada");
+    }
+    catch (ClaveYaExistenteException e)
+    {
+      Assert.fail("La clave no deberia existir");
+    }
+    catch (DatoInvalidoException e)
+    {
+      Assert.fail("El dato no deberia ser invalido");
+    }
+    catch (Exception e)
+    {
+    }
+  }
+}
---
+++ TPFinal/src/test/caja_negra/BajaAlummnoEnCursadaTest.java
@@ -0,0 +1,110 @@
+package test.caja_negra;
+
+import org.junit.After;
+import org.junit.Assert;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BajaAlummnoEnCursadaTest
+{
+  SistemaConElementos fixture1 = new SistemaConElementos();
+
+  public BajaAlummnoEnCursadaTest()
+  {
+  }
+
+  @Before
+  public void setUp()
+    throws Exception
+  {
+    fixture1.setUp();
+    this.fixture1
+        .alumno
+        .eliminarHistoria(this.fixture1.asignatura);
+    this.fixture1
+        .sistema
+        .agregarAlumnoEnCursada(this.fixture1.alumno, this.fixture1.cursada);
+  }
+
+  @After
+  public void tearDown()
+    throws Exception
+  {
+    fixture1.tearDown();
+  }
+
+  /**
+   * @see modelo.Cursada#bajaAlumno(modelo.Alumno)
+   */
+  @Test
+  public void testBajaAlumnoCorrecto()
+  {
+    this.fixture1
+        .cursada
+        .bajaAlumno(this.fixture1.alumno);
+    Assert.assertFalse("El alumno deberia dejar de estar en la cursada", this.fixture1
+                                                                             .cursada
+                                                                             .tieneAlumno(this.fixture1.alumno));
+  }
+
+  /**
+   * @see modelo.Cursada#bajaAlumno(modelo.Alumno)
+   */
+  @Test
+  public void testBajaAlumnoErroneo2()
+  {
+    try
+    {
+      this.fixture1
+          .cursada
+          .bajaAlumno(null);
+      Assert.fail("El alumno no deberia poder ser eliminado");
+    }
+    catch (Exception e)
+    {
+    }
+  }
+
+  /**
+   * @see modelo.Cursada#bajaAlumno(modelo.Alumno)
+   */
+  @Test
+  public void testBajaAlumnoErroneo4()
+  {
+    try
+    {
+      this.fixture1
+          .sistema
+          .setAlumnos(null);
+      this.fixture1
+          .cursada
+          .bajaAlumno(this.fixture1.alumno);
+      Assert.fail("El alumno no deberia poder ser eliminado");
+    }
+    catch (Exception e)
+    {
+    }
+  }
+
+  /**
+   * @see modelo.Cursada#bajaAlumno(modelo.Alumno)
+   */
+  @Test
+  public void testBajaAlumnoErroneo6()
+  {
+    this.fixture1
+        .sistema
+        .eliminarAlumno(this.fixture1.alumno);
+    try
+    {
+      this.fixture1
+          .cursada
+          .bajaAlumno(this.fixture1.alumno);
+      Assert.fail("El alumno no deberia poder ser eliminado");
+    }
+    catch (Exception e)
+    {
+    }
+  }
+}
